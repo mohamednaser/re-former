a6381c1b71f58ea7cfb18492c0febe6d0b9f00cf
Mohamed Naser <nc_m.naser@hotmail.com>
2019-12-09 19:49:03 +0200

    fix user_params issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,10 +23,8 @@ class UsersController < ApplicationController
   def edit; end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email],
-                     password: params[:password])
+    @user = User.new(user_params)
 
-    
     if @user.save
       redirect_to new_user_path
     else
